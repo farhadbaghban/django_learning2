1736f40e8218a576d27571e9a87514b3b3c8b89f
farhadbaghban <baghbanfarhad@gmail.com>
2022-12-08 15:03:14 +0330

    Registration User

--- account/views.py
+++ account/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from django.views import View
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 class UserRegistrationView(View):
+    form_class = RegistrationForm
+    template_class = "account/registeration.html"
+
     def get(self, request):
-        form = RegistrationForm()
-        return render(request, "account/registeration.html", {"form": form})
+        form = self.form_class()
+        return render(request, self.template_class, {"form": form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd["username"], cd["email"], cd["password"])
+            messages.success(request, "Your registered SuccessFully", "success")
+            return redirect("home:index")
+        else:
+            messages.error(request, "Enter Valid Data", "warning")
+            return render(request, self.template_class, {"form": form})
--- django_learning2/urls.py
+++ django_learning2/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from argparse import Namespace
 from django.contrib import admin
 from django.urls import path, include
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("account/", include("account.urls")),
-    path("", include("home.urls")),
+    path("account/", include("account.urls", namespace="account")),
+    path("", include("home.urls", namespace="home")),
 ]
