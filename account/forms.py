f43f7c6db63dc0d6c922a79b57236242cbd2e610
farhadbaghban <baghbanfarhad@gmail.com>
2022-12-08 18:29:38 +0330

    clean Overwriting

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 import email
 from genericpath import exists
+from os import pipe2
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -8,22 +9,43 @@ from django.core.exceptions import ValidationError
 class RegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control"})
+    password1 = forms.CharField(
+        label="Password", widget=forms.PasswordInput(attrs={"class": "form-control"})
+    )
+    password2 = forms.CharField(
+        label="confirm Password",
+        widget=forms.PasswordInput(attrs={"class": "form-control"}),
     )
 
-    def clean_email(self):
-        email = self.cleaned_data["email"]
-        user = User.objects.filter(email=email).exists()
-        if user:
-            raise ValidationError("This Email Already Exists")
-        return email
+    # def clean_email(self):
+    #     email = self.cleaned_data["email"]
+    #     user = User.objects.filter(email=email).exists()
+    #     if user:
+    #         raise ValidationError("This Email Already Exists")
+    #     return email
 
-    def clean_username(self):
-        username = self.cleaned_data["username"]
+    # def clean_username(self):
+    #     username = self.cleaned_data["username"]
+    #     user = User.objects.filter(username=username).exists()
+    #     if user:
+    #         raise ValidationError(
+    #             "Please Change The Username, This Username Already Exists"
+    #         )
+    #     return username
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get("password1")
+        p2 = cd.get("password2")
+        email = cd.get("email")
+        username = cd.get("username")
         user = User.objects.filter(username=username).exists()
-        if user:
+        email = User.objects.filter(email=email).exists()
+        if p1 and p2 and p1 != p2:
+            raise ValidationError("Passwords must be Match")
+        elif user:
             raise ValidationError(
                 "Please Change The Username, This Username Already Exists"
             )
-        return username
+        elif email:
+            raise ValidationError("This Email Already Exists")
--- account/views.py
+++ account/views.py
@@ -18,7 +18,7 @@ class UserRegistrationView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd["username"], cd["email"], cd["password"])
+            User.objects.create_user(cd["username"], cd["email"], cd["password1"])
             messages.success(request, "Your registered SuccessFully", "success")
             return redirect("home:index")
         else:
