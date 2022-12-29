a57016669f1da0146937dea91040b1ff848c39b8
farhadbaghban <baghbanfarhad@gmail.com>
2022-12-29 07:05:46 +0330

    Extended User Model

--- account/models.py
+++ account/models.py
@@ -13,3 +13,9 @@ class Relation(models.Model):
 
     def __str__(self):
         return f"{self.from_user} followed {self.to_user}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveSmallIntegerField(default=0)
+    bio = models.CharField(max_length=400, null=True, blank=True)
